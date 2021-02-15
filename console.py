4678712b105267f51e8e1627e12485cbd8067282
ANDRESOTELO <andresotelo2020@gmail.com>
2021-02-15 20:40:30 +0000

    Update create method

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 Program that contains the entry point of the command interpreter
 """
 
+import sys
+import models
+import json
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 import cmd
 
 
@@ -10,25 +15,46 @@ class HBNBCommand(cmd.Cmd):
     """
     Commnad interpreter class definition
     """
+    intro = "Welcome to Airbnb console! Type ? or help to list commands"
     prompt = "(hbnb)"
 
-    def do_quit(self, command):
-        """Command to EXIT program
+    def do_quit(self, line):
+        """Exit the program writing 'quit'
         """
         return True
 
-    def do_EOF(self, command):
-        """Command to end program
+    def do_EOF(self, line):
+        """Exit the program with Ctrl + D
         """
         return True
 
     def emptyline(self):
-        """Do not execute anything with an empty line + ENTER """
+        """Empty line is entered in response to the prompt
+        """
         return
 
-    def _create(self, command)
-        """Command to create a new instance
+    def do_create(self, line):
+        """Create a new instance of BaseModel. If instance was created,
+the id of the instance will be printed
         """
+        if len(line) < 1:
+            print("** class name missing **")
+        elif line == "BaseModel":
+            obj = BaseModel()
+            obj.save()
+            print(obj.id)
+        else:
+            print("** class doesn't exist **")
+
+    def __str__(self):
+        """Method to override the __str__ method and return
+specific text format
+        """
+        return "{} {}".format(obj.__class__.__name__, obj.id)
+
 
 if __name__ == "__main__":
+    """
+    To execute program like a main
+    """
     HBNBCommand().cmdloop()
