c7815240e831d560245b1d19f8f5edb10e8a8425
Jong9106 <juank_hr@hotmail.com>
2021-02-16 20:04:11 -0500

    New Class

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to create a new class
+"""
+
+import models
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """ 
+    Class City that inherits from BaseModel
+    """
+    state_id = ""
+    name = ""
