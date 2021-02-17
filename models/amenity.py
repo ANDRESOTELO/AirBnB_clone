e1f912b3176d0f53b0b5399e0c6333ab79e24814
Jong9106 <juank_hr@hotmail.com>
2021-02-16 20:04:23 -0500

    New Class

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module to create a new class
+"""
+
+import models
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ 
+    Class Amenity that inherits from BaseModel
+    """
+    name = ""
