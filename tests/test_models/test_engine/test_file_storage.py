91d1fde657d41580c4fd2233f526cd4642041edc
ANDRESOTELO <andresotelo2020@gmail.com>
2021-02-18 01:42:45 +0000

    kwargs to create an instance

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -132,3 +132,14 @@ class TestFileStorage(unittest.TestCase):
             delete("file.json")
         except:
             pass
+
+    def test_kwargs(self):
+        """
+        Test to check reload function
+        """
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        my_new_model = BaseModel(**my_model_json)
+        self.assertIsNot(my_model, my_new_model)
